9fded0171ad2229b58eed25f55638820009bd1d8
ChrisBWilliams9 <112098145+ChrisBWilliams9@users.noreply.github.com>
2026-08-19 15:56:14 -0500

    Add: last-stone-weight - submission-2

---
+++ Data Structures & Algorithms/last-stone-weight/submission-2.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int lastStoneWeight(int[] stones) {
+        PriorityQueue<Integer> maxHeap = new PriorityQueue<>(Comparator.reverseOrder());
+
+        for (int stone : stones) {
+            maxHeap.offer(stone);
+        }
+
+        while (maxHeap.size() >= 2) {
+            int x = maxHeap.poll();
+            int y = maxHeap.poll();
+
+            if (x > y) {
+                maxHeap.offer(x - y);
+            } else {
+                continue;
+            }
+        }
+
+        if (maxHeap.isEmpty()) {
+            return 0;
+        }
+
+        return maxHeap.peek();
+    }
+}
