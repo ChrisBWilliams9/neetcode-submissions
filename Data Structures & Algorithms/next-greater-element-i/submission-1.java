7a0b7f16137f2237671d3c80b84ae22034e3a429
ChrisBWilliams9 <112098145+ChrisBWilliams9@users.noreply.github.com>
2026-08-25 14:31:21 -0500

    Add: next-greater-element-i - submission-1

---
+++ Data Structures & Algorithms/next-greater-element-i/submission-1.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int[] nextGreaterElement(int[] nums1, int[] nums2) {
+        HashMap<Integer, Integer> nextGreater = new HashMap<>();
+        Stack<Integer> stack = new Stack<>();
+
+        for (int num : nums2) {
+
+            while (!stack.isEmpty() && num > stack.peek()) {
+                int smaller = stack.pop();
+                nextGreater.put(smaller, num);
+            }
+
+            stack.push(num);
+        }
+
+        int[] result = new int[nums1.length];
+
+        for (int i = 0; i < nums1.length; i++) {
+            result[i] = nextGreater.getOrDefault(nums1[i], -1);
+        }
+
+        return result;
+    }
+}
+
